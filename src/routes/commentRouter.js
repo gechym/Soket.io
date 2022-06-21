9dc94303fdfe2eb6746592d85586f8907c7401f7
ndbao <nguyenducbao166@gmail.com>
2022-06-21 19:54:47 +0700

    Deploy heroku

--- src/routes/commentRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getComments } from '../controller/commentController';
-
-const commentRouter = express.Router();
-
-commentRouter.route('/:id').get(getComments);
-
-export default commentRouter;
--- src/routes/productRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getProduct, reviews } from '../controller/productControler';
-
-const productRouter = express.Router();
-
-productRouter.route('/').get(getProduct);
-productRouter.route('/:id').patch(reviews);
-
-export default productRouter;
